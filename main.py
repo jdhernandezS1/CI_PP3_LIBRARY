d09e028418677dc3619c9dfd2670314a32b911dd
David <julio-david@hotmail.com>
2022-07-11 22:36:30 +0200

    Testing return book  function Delete client of data base

--- main.py
+++ main.py
@@ -77,12 +77,15 @@ def returnBook(books,inventary):
         monthLimit = int(clientDate[0])
         yearLimit = int(clientDate[1])
         actualDate = datetime.datetime.now() #actual date
-        month = int(actualDate.month+2)
+        month = int(actualDate.month)
         year = int(actualDate.year)
-        if (monthLimit >= month and yearLimit >= year)or (yearLimit >= year) :# condition if the book is on correct date
+        if (monthLimit >= month and yearLimit >= year)or (yearLimit > year) :# condition if the book is on correct date
             phrase= "The Book was returned on time"
         else :
-            phrase= "The client is not in time the tax of extra time is:"
+            if yearLimit < year:
+                month+=12*abs(yearLimit-year)
+            tax= (abs(monthLimit-month)*20) #calculate the tax
+            phrase= "The client is not in time the tax of extra time is:"+ str(tax) + " $" # To let know at the staff the tax
         if cellStock!=None: #verify if we have it
             stockRow = cellStock.row  # # of the row where is the book 
             stockCol = cellStock.col    # # of the column where is the book 
@@ -91,13 +94,15 @@ def returnBook(books,inventary):
             condition = (stock!= stock-rented) #the condition of the if store have to be more than avaiable or we not have dispnoibility        
             if (condition) :
                 val = rented-1
-                # books.update_cell(8, newCol, str(val))
+                books.update_cell(8, stockCol, str(val))#add 1 value to the stock 
+                inventary.update_cell(x,col,"")#deleting the client of the data base
+                inventary.update_cell(x,col+1,"")#deleting the client of the data base
                 print(phrase)
             else:
-                print("All the copies of the book are in the store")
+                print("All the copies of the book are in the store") #The stock is full 
     else:
-        print("Sry but we dont have any book with that name.")
+        print("Sry but we dont have any book with that name.") # is not a book of the library
 
 inventaryValues = inventary.get_all_values()#get values of inventary 
 # rentBook(books,inventary)
-returnBook(books,inventary)
+# returnBook(books,inventary)
