beb7659367a41e148fc480cb5f80fba886668253
David <julio-david@hotmail.com>
2022-07-19 19:51:47 +0200

    pep8 Errors fixing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import gspread
 import os
 import functions as fs
-from google.oauth2.service_account import Credentials 
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -10,24 +10,29 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 # Initial declarations
-CREDS = Credentials.from_service_account_file('creds.json')#conect with the json file to google sheets
-SCOPED_CREDS =  CREDS.with_scopes(SCOPE) 
+CREDS = Credentials.from_service_account_file('creds.json')
+#  conect with the json file to google sheets
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Library') # library folder
-inventary = SHEET.worksheet('inventary') #inventary sheet
-books = SHEET.worksheet('books')# books sheet
-cred = SHEET.worksheet('cred')# credentials sheet
+SHEET = GSPREAD_CLIENT.open('Library')
+# library folder
+inventary = SHEET.worksheet('inventary')
+# inventary sheet
+books = SHEET.worksheet('books')
+# books sheet
+cred = SHEET.worksheet('cred')
+# credentials sheet
 while True:
     flag = False
     fs.clear_console()
     print("\033[1;32;80m")
 
-    try :
+    try:
         flag = fs.login(cred)
     except:
         print("exeption please try Reload the page")
-    while flag:   
-        fs.clear_console()     
+    while flag:
+        fs.clear_console()
         fs.wellcomeMessage()
         print("\033[1;32;80m")
         print("\n Please type an option and press enter:")
@@ -35,19 +40,16 @@ while True:
         print("\n 2 To return a book ")
         print("\n 3 To get client information")
         option = input("\n 4 To log out \n\n Option :")
-        
-        if(option=="1"):
+        if(option == "1"):
             fs.clear_console()
-            fs.rentBook(books,inventary)
-        
-        elif(option=="2"):
+            fs.rentBook(books, inventary)
+        elif(option == "2"):
             fs.clear_console()
-            fs.returnBook(books,inventary)
-        
-        elif(option=="3"):
+            fs.returnBook(books, inventary)
+        elif(option == "3"):
             fs.clear_console()
-            fs.findClient(books,inventary)
-        elif(option=="4"):
+            fs.findClient(books, inventary)
+        elif(option == "4"):
             print("Please Press Reload button to start again")
             quit()
         else:
