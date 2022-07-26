e758ccf455ce65e5396381c80c0785b72d0379d4
David <julio-david@hotmail.com>
2022-07-26 23:19:18 +0200

    unittest functions testing

--- test_functions.py
+++ test_functions.py
@@ -2,7 +2,6 @@ import unittest
 import functions as fs
 import validation as val
 
-
 class TestCred(unittest.TestCase):
     """
     Verification Credentials
@@ -13,3 +12,28 @@ class TestCred(unittest.TestCase):
 
     def test_validate_credentials_False(self):
         self.assertFalse(val.test_login(val.cred,"test","test"))
+
+
+class TestFuncs(unittest.TestCase):
+    """
+    Verification Functions
+    are working
+    """
+    def test_rent_book(self):
+        self.assertNotIsInstance(fs.rentBook(val.books, val.inventary))
+
+    def test_return_book(self):
+        self.assertNotIsInstance(fs.returnBook(val.books, val.inventary))
+        
+    def test_find_client(self):
+        self.assertNotIsInstance(fs.findClient(val.books, val.inventary))
+
+
+class Test_wellcomeMessage(unittest.TestCase):
+    """
+    Verification wellcome
+    message
+    """
+    def test_message(self):
+        self.assertNotIsInstance(fs.wellcomeMessage())
+        
